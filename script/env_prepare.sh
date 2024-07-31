2299170d09ddde891e99dd409e3d2a64f7402afe
prgrmz07 <prgrmz07@163.com>
2024-07-31 10:00:34 +0800

    更新 script/env_prepare.sh

--- script/env_prepare.sh
+++ script/env_prepare.sh
@@ -8,6 +8,7 @@ source .venv/bin/activate
 #pip install pmbootstrap #报错信息中有版本列表，现在是2024年7月31日，最新版本是2.1.0
 # pip install pmbootstrap==2.1.0
 
+#从源码安装pmbootstrap参考 https://wiki.postmarketos.org/wiki/Pmbootstrap#From_git
 git -b 2.3.0 clone https://gitlab.com/postmarketOS/pmbootstrap.git /app2/
 sudo ln -s "/app2/pmbootstrap/pmbootstrap.py" /usr/bin/pmbootstrap
 which pmbootstrap #/usr/bin/pmbootstrap
